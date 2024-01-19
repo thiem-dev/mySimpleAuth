4bfe8c65f4edb4b7b34b5a093ea6c512d32cfe27
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-19 15:57:21 -0500

    login pw bcrypt compare update

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -11,7 +11,7 @@ router.post('/register', async (req, res) => {
         //check if user already exists
         const user = await pool.query('SELECT * FROM users WHERE user_email = $1;', [email])
         if(user.rows.length !== 0){
-            res.status(402).send('user alerady exists')
+            res.status(402).send('user already exists')
         }
 
         const saltRounds = 10;
@@ -31,5 +31,33 @@ router.post('/register', async (req, res) => {
         res.status(500).send('server errorr...')
     }
 });
+
+router.post('/login', async(req,res) => {
+    try{
+        const {email, password} = req.body
+
+        const user = await pool.query('SELECT * FROM users WHERE user_email = $1;', [email])
+        
+        if(user.rows.length < 1){
+            return res.status(404).send('User not found...')
+        }
+
+        //check if inc password is the same as the db password
+        const validPassword = await bcrypt.compare(password, user,rows[0].user_password)
+
+        if(!validPassword){
+            return res.status(401).send('Name or email is wrong...')
+        }
+
+        //if valid, give jwt
+        const token = jwtGenerator(newUser.rows[0].user_id)
+
+        res.json({token})
+
+    } catch (error){
+        console.error(error.message)
+        res.status(500).send('Server error')
+    }
+})
 
 module.exports = router
